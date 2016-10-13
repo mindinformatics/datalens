c8f241ea2763b7a0785b2c4e30cc2c99c0be8673
Emily Merrill <memerrill@gmail.com>
2016-10-13 16:54:30 -0400

    Updates.

--- sites/all/libraries/d3.bubblechart/bubblechart.js
+++ sites/all/libraries/d3.bubblechart/bubblechart.js
@@ -29,9 +29,17 @@
         .append("g")
         .attr("transform", "translate(2,2)");
 
-      d3.csv("/sites/all/themes/scf_theme/BubbleChart/test.csv", function(error, data) {
+      var arc = d3.svg.arc()
+        .innerRadius(180)
+        .outerRadius(240)
+        .startAngle(180)
+        .endAngle(2*Math.PI);
+
+      d3.csv("/sites/all/themes/scf_theme/BubbleChart/Hypoxia-viz.csv", function(error, data) {
           data.forEach(function(d) {
             d.LogFC = +d.LogFC;
+            d.PValue = +d.PValue;
+            d.AdjPValue = +d.AdjPValue;
           });
 
 
@@ -41,6 +49,8 @@
           map[node.name] = node;
           return map;
         }, {});
+
+        //console.debug(dataMap);
 
         // create the tree array
         var treeData = [];
@@ -73,30 +83,46 @@
 
           node.append("title")
                 //(d.children ? "" : ": " + format(d.size)
-                .html(function(d) { return (d.children ? d.name : d.Gene + "<br/>" + "FC: " + d.LogFC + "<br/>" + "P-value: " + d.PValue + "<br/>" + "Adjusted P-value: " + d.AdjPValue + "<br/>" + "Study: " + d.Study) });
+                .html(function(d) { return (d.children ? d.name : d.name + "<br/>" + "FC: " + d.LogFC + "<br/>" + "P-value: " + d.PValue + "<br/>" + "Adjusted P-value: " + d.AdjPValue + "<br/>" + "Study: " + d.Study) });
 
 
           var data_points = d3.entries(dataMap);
           //console.debug(test);
           var max_fc = d3.max( data_points, function(d) { return d['value']['LogFC'] });
-          //console.debug(max_fc);
+          console.debug(max_fc);
           var min_fc = d3.min( data_points, function(d) { return d['value']['LogFC'] });
-          //console.debug(min_fc);
+          console.debug(min_fc);
           var color_scale = d3.scale.linear().domain([min_fc, max_fc]).range(['#253494', '#bd0026']);
           console.debug(color_scale(max_fc));
 
           node.append("circle")
               .attr("r", function(d) { return d.r; })
               .style('fill', function(d) { return (d.children ? "none" : color_scale(d.LogFC)); })
-              .style('fill-opacity', function(d) { return (d.children ? '0' : '.5'); })
-
+              .style('fill-opacity', function(d) { return (d.children ? '0' : '.6'); })
               .style('stroke', 'black');
 
 
+          node.append("path")
+              .attr("fill","none")
+              .attr("d", arc);
+
+          console.debug(node);
+
+          //If no children, display title like this
           node.filter(function(d) { return !d.children; }).append("text")
               .attr("dy", ".3em")
               .style("text-anchor", "middle")
               .text(function(d) { return d.name.substring(0, d.r / 3); });
+
+
+        //If has children
+         node.filter(function(d) { return d.children; }).append("text")
+              .attr("dy", "10em")
+              //.append("textPath")
+              //.attr("xlink:href",function(d,i){return "#s"+i;})
+              //.attr("startOffset",function(d,i){return "25%";})
+              .style("text-anchor", "left")
+              .text(function(d) { return d.name; });
         });
 
         d3.select(self.frameElement).style("height", diameter + "px");

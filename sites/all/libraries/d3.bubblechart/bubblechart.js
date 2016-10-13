e8c9662130dd722ed920ac50c84172b5c180872d
Emily Merrill <memerrill@gmail.com>
2016-10-13 14:18:19 -0400

    Updates to script.

--- sites/all/libraries/d3.bubblechart/bubblechart.js
+++ sites/all/libraries/d3.bubblechart/bubblechart.js
@@ -30,6 +30,10 @@
         .attr("transform", "translate(2,2)");
 
       d3.csv("/sites/all/themes/scf_theme/BubbleChart/test.csv", function(error, data) {
+          data.forEach(function(d) {
+            d.LogFC = +d.LogFC;
+          });
+
 
         // *********** Convert flat data into a nice tree ***************
         // create a name: node map
@@ -55,22 +59,39 @@
         });
 
         root = treeData[0];
+
+
+
         var node = svg.datum(root).selectAll(".node")
                 .data(pack.nodes);
 
-                console.debug(node);
+                //console.debug(node);
 
           node.enter().append("g")
                 .attr("class", function(d) { return d.children ? "node" : "leaf node"; })
                 .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
           node.append("title")
-                //Gene,Log(FC),P.Value,Adj.P.Value
                 //(d.children ? "" : ": " + format(d.size)
-                .html(function(d) { return (d.children ? d.name : d.Gene + " <br/> " + "FC: " + d.LogFC + " <br/> " + "P.value: " + d.PValue +" <br/> " + d.Study) });
+                .html(function(d) { return (d.children ? d.name : d.Gene + "<br/>" + "FC: " + d.LogFC + "<br/>" + "P-value: " + d.PValue + "<br/>" + "Adjusted P-value: " + d.AdjPValue + "<br/>" + "Study: " + d.Study) });
+
+
+          var data_points = d3.entries(dataMap);
+          //console.debug(test);
+          var max_fc = d3.max( data_points, function(d) { return d['value']['LogFC'] });
+          //console.debug(max_fc);
+          var min_fc = d3.min( data_points, function(d) { return d['value']['LogFC'] });
+          //console.debug(min_fc);
+          var color_scale = d3.scale.linear().domain([min_fc, max_fc]).range(['#253494', '#bd0026']);
+          console.debug(color_scale(max_fc));
 
           node.append("circle")
-              .attr("r", function(d) { return d.r; });
+              .attr("r", function(d) { return d.r; })
+              .style('fill', function(d) { return (d.children ? "none" : color_scale(d.LogFC)); })
+              .style('fill-opacity', function(d) { return (d.children ? '0' : '.5'); })
+
+              .style('stroke', 'black');
+
 
           node.filter(function(d) { return !d.children; }).append("text")
               .attr("dy", ".3em")

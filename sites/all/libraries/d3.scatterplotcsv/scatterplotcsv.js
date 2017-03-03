31dde39921ec7edf5b0f339472ad8a2e27bd8f2d
Emily Merrill <memerrill@gmail.com>
2017-03-03 16:01:34 -0500

    Data and files for scatterplot.

--- sites/all/libraries/d3.scatterplotcsv/scatterplotcsv.js
+++ sites/all/libraries/d3.scatterplotcsv/scatterplotcsv.js
@@ -18,13 +18,81 @@
       //rows = settings.rows;
       //console.debug(rows);
 
-      d3.csv("/sites/all/themes/scf_theme/Scatterplot/file.csv", function(error, data) {
-        console.debug(data);
-          data.forEach(function(d) {
-            d.LogFC = +d.LogFC;
-            d.PValue = +d.PValue;
-            d.AdjPValue = +d.AdjPValue;
+      //Data: AMP-AD_MSBB_APOE_Tau.csv: Geneid,Chr,Start,End,Strand,Length,geneSymbol,hB_RNA_1111
+      //Clinical: MSBB_clinical.csv: individualIdentifier,PMI,RACE,AOD,CDR,SEX,NP.1,PlaqueMean,bbscore
+      //MSBB_SampleIDs.csv: SampleID, SubjectID, BrainRegion
+
+      //<script src="/lib/d3.v4.3.0.js"></script>
+      //<script src="http://dimplejs.org/dist/dimple.v2.3.0.min.js"></script>
+    var svg = dimple.newSvg("#chartContainer", 590, 400);
+      d3.csv("/sites/all/themes/scf_theme/Scatterplot/dclk1_data.csv", function (data) {
+        // Create the chart
+        var myChart = new dimple.chart(svg, data);
+        myChart.setBounds(60, 30, 420, 330)
+
+        // Create a standard bubble of SKUs by Price and Sales Value
+        // We are coloring by Owner as that will be the key in the legend
+        //myChart.addMeasureAxis("x", "AgeAtDeath");
+        myChart.addCategoryAxis("x", ["Diagnosis"]);
+        myChart.addMeasureAxis("y", "ENSG00000133083");
+        myChart.addSeries(["Sex", "ApoE", "AgeAtDeath"], dimple.plot.bubble);
+        var myLegend = myChart.addLegend(530, 100, 60, 300, "Right");
+        myChart.draw();
+
+        // This is a critical step.  By doing this we orphan the legend. This
+        // means it will not respond to graph updates.  Without this the legend
+        // will redraw when the chart refreshes removing the unchecked item and
+        // also dropping the events we define below.
+        myChart.legends = [];
+
+        // This block simply adds the legend title. I put it into a d3 data
+        // object to split it onto 2 lines.  This technique works with any
+        // number of lines, it isn't dimple specific.
+        svg.selectAll("title_text")
+          .data(["Click legend to","show/hide ApoE:"])
+          .enter()
+          .append("text")
+            .attr("x", 499)
+            .attr("y", function (d, i) { return 90 + i * 14; })
+            .style("font-family", "sans-serif")
+            .style("font-size", "10px")
+            .style("color", "Black")
+            .text(function (d) { return d; });
+
+        // Get a unique list of Owner values to use when filtering
+        var filterValues = dimple.getUniqueValues(data, "ApoE");
+        // Get all the rectangles from our now orphaned legend
+        myLegend.shapes.selectAll("rect")
+          // Add a click event to each rectangle
+          .on("click", function (e) {
+            // This indicates whether the item is already visible or not
+            var hide = false;
+            var newFilters = [];
+            // If the filters contain the clicked shape hide it
+            filterValues.forEach(function (f) {
+              if (f === e.aggField.slice(-1)[0]) {
+                hide = true;
+              } else {
+                newFilters.push(f);
+              }
+            });
+            // Hide the shape or show it
+            if (hide) {
+              d3.select(this).style("opacity", 0.2);
+            } else {
+              newFilters.push(e.aggField.slice(-1)[0]);
+              d3.select(this).style("opacity", 0.8);
+            }
+            // Update the filters
+            filterValues = newFilters;
+            // Filter the data
+            myChart.data = dimple.filterData(data, "ApoE", filterValues);
+            // Passing a duration parameter makes the chart animate. Without
+            // it there is no transition
+            myChart.draw(800);
           });
+      });
+  </script>
 
 
 })(jQuery);
---
+++ sites/all/themes/scf_theme/Scatterplot/getCSV.R
@@ -0,0 +1,64 @@
+setwd("~/Dropbox (Partners HealthCare)/MSBB/Pipelines/Data/MayoBB/CBE")
+options(stringsAsFactors = FALSE)
+library(data.table)
+
+
+clinical = read.csv("MayoRNAseq_RNAseq_CBE_covariates.csv", header=T, stringsAsFactors=F)
+dim(clinical)
+
+#fread for large files
+dataFile = fread("MayoRNAseq_RNAseq_CBE_geneCounts_normalized.tsv")
+colnames(dataFile)
+head(dataFile[1,])
+dim(dataFile)
+
+# Remove covariate information not also in dataFile
+clinical_removes = subset(clinical, !(clinical$SampleID %in% colnames(dataFile)))
+clinical_keeps = subset(clinical, (clinical$SampleID %in% colnames(dataFile)))
+clinical = clinical_keeps
+
+#Find NAs- keep this in mind for data clean up later.
+#clinical[is.na(clinical$Source),]
+#> clinical[is.na(clinical$Source),]
+#SampleID Source Tissue RIN Diagnosis  Sex AgeAtDeath ApoE Flowcell
+#95  11386_CER   <NA>   <NA>  NA      <NA> <NA>       <NA>   NA     <NA>
+#244   742_CER   <NA>   <NA>  NA      <NA> <NA>       <NA>   NA     <NA>
+
+row.names(clinical) = clinical$SampleID
+clinical = t(clinical)
+ensembl_id = row.names(clinical)
+clinical = cbind(ensembl_id,clinical)
+clinical = as.data.frame(clinical)
+dropme = c("RIN","Flowcell")
+clinical = clinical[!row.names(clinical) %in% dropme,]
+
+
+# Get row of interest
+#DCLK1: ENSG00000133083
+dclk1_rows = dataFile[dataFile$ensembl_id == "ENSG00000133083",]
+dclk1_dat = rbind(clinical, dclk1_rows)
+dim(dclk1_dat)
+
+# final data clean up- get rid of NAs ingering from clinical, and make easy to read.
+dclk1_dat = t(dclk1_dat)
+dclk1_dat = as.data.frame(dclk1_dat)
+colnames(dclk1_dat) = dclk1_dat[1,]
+dclk1_dat = dclk1_dat[!is.na(dclk1_dat$Source),]
+dclk1_dat = dclk1_dat[-c(1),]
+write.table(dclk1_dat, file="dclk1_data.csv", sep=",", row.names=FALSE, col.names=TRUE, quote=TRUE)
+
+#APP: ENSG00000142192
+app_rows = dataFile[dataFile$ensembl_id == "ENSG00000142192",]
+app_dat = rbind(clinical,app_rows)
+app_dat = t(app_dat)
+app_dat = as.data.frame(app_dat)
+colnames(app_dat) = app_dat[1,]
+app_dat = app_dat[!is.na(app_dat$Source),]
+app_dat = app_dat[-c(1),]
+
+write.table(app_dat, file="app_data.csv", sep=",", row.names=FALSE, col.names=TRUE, quote=TRUE)
+
+
+
+
+

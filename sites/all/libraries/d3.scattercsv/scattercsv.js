e678c8e6815d0a2c25ff38d5ebac09f36a8298ed
Sudeshna Das <sudeshna.das2@gmail.com>
2017-04-19 12:22:59 -0400

    Scatter csv changes

--- sites/all/libraries/d3.scattercsv/scattercsv.js
+++ sites/all/libraries/d3.scattercsv/scattercsv.js
@@ -6,7 +6,8 @@
 
 Drupal.d3.scattercsv = function (select, settings) {
 
-      var svg = dimple.newSvg("#visualization", 900, 600);
+  /*
+    var svg = dimple.newSvg("#visualization", 900, 600);
       var real_fc  = function(x){ return (x>0 ? Math.pow(2,x) : -1/Math.pow(2,x)) };
 
       d3.csv("/sites/all/libraries/d3.scattercsv/MSBB_HIPP_Braak_CERAD.csv", function (data) {
@@ -27,11 +28,161 @@ Drupal.d3.scattercsv = function (select, settings) {
         myXAxis.colors = ["#DA9694"];
         myYAxis.colors = ["#DA9694"];
         myChart.addColorAxis("logFC", "#000000")
-        myChart.addColorAxis("GeneSymbol", "#000000")
         myChart.addSeries(["GeneSymbol", "PValue", "PValue1"], dimple.plot.bubble);
         //myChart.addLegend(200, 10, 360, 20, "right");
         myChart.draw();
       });
+ */
+var margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var x = d3.scale.linear()
+    .range([0, width]);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var color = d3.scale.category10();
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var svg = d3.select('#' + settings.id).append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// Lasso functions to execute while lassoing
+var lasso_start = function() {
+  lasso.items()
+    .attr("r",3.5) // reset size
+    .style("fill",null) // clear all of the fills
+    .classed({"not_possible":true,"selected":false}); // style as not possible
+};
+
+var lasso_draw = function() {
+  // Style the possible dots
+  lasso.items().filter(function(d) {return d.possible===true})
+    .classed({"not_possible":false,"possible":true});
+
+  // Style the not possible dot
+  lasso.items().filter(function(d) {return d.possible===false})
+    .classed({"not_possible":true,"possible":false});
+};
+
+var lasso_end = function() {
+  // Reset the color of all dots
+  lasso.items()
+     .style("fill", function(d) { return "red"; });
+
+  // Style the selected dots
+  lasso.items().filter(function(d) {return d.selected===true})
+    .classed({"not_possible":false,"possible":false})
+    .attr("r",7);
+
+  // Reset the style of the not selected dots
+  lasso.items().filter(function(d) {return d.selected===false})
+    .classed({"not_possible":false,"possible":false})
+    .attr("r",3.5);
+
+};
+
+// Create the area where the lasso event can be triggered
+var lasso_area = svg.append("rect")
+                      .attr("width",width)
+                      .attr("height",height)
+                      .style("opacity",0);
+
+// Define the lasso
+var lasso1 = d3.lasso()
+      .closePathDistance(75) // max distance for the lasso loop to be closed
+      .closePathSelect(true) // can items be selected by closing the path?
+      .hoverSelect(true) // can items by selected by hovering over them?
+      .area(lasso_area) // area where the lasso can be started
+      .on("start",lasso_start) // lasso start function
+      .on("draw",lasso_draw) // lasso draw function
+      .on("end",lasso_end); // lasso end function
+
+
+
+// Init the lasso on the svg:g that contains the dots
+console.debug("lasso1");
+console.debug(lasso1);
+d3.select("svg").call(lasso1);
+
+d3.csv("/sites/all/libraries/d3.scattercsv/MSBB_HIPP_Braak_CERAD.csv", function(error, data) {
+  data.forEach(function(d) {
+    d.logFC = +d.logFC;
+    d.logFC1 = +d.logFC1;
+  });
+
+  console.debug("data");
+  console.debug(data);
+
+  x.domain(d3.extent(data, function(d) { return d.logFC; })).nice();
+  y.domain(d3.extent(data, function(d) { return d.logFC1; })).nice();
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("x", width)
+      .attr("y", -6)
+      .style("text-anchor", "end")
+      .text("log(FC) using Braak scores");
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("class", "label")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("log(FC) using Cerad scores")
+
+  svg.selectAll(".dot")
+      .data(data)
+    .enter().append("circle")
+      .attr("id",function(d,i) {return "dot_" + i;}) // added
+      .attr("class", "dot")
+      .attr("r", 3.5)
+      .attr("cx", function(d) { return x(d.logFC); })
+      .attr("cy", function(d) { return y(d.logFC1); })
+      .style("fill", function(d) { return "black"; });
+
+  lasso.items(d3.selectAll(".dot"));
+
+  var legend = svg.selectAll(".legend")
+      .data(color.domain())
+    .enter().append("g")
+      .attr("class", "legend")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+
+  legend.append("rect")
+      .attr("x", width - 18)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", color);
+
+  legend.append("text")
+      .attr("x", width - 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(function(d) { return d; });
+
+});
 
 }
 

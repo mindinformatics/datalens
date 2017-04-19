dc94a880db878985813607ec89c302b96b2e15f5
Emily Merrill <memerrill@gmail.com>
2017-04-19 15:05:40 -0400

    Script for R stuff.

--- sites/all/libraries/d3.scattercsv/Script.R
+++ sites/all/libraries/d3.scattercsv/Script.R
@@ -7,4 +7,11 @@ head(dat)
 
 
 dat1 <- dat[((dat$PValue < 0.01) | (dat$PValue1 < 0.01)),]
-
+write.table(dat1, file="MSBB_HIPP_Braak_CERAD_Pval.csv", sep=",", row.names=FALSE, col.names=TRUE, quote=FALSE)
+max(dat1$logFC)
+min(dat1$logFC)
+dat1$color[dat1$logFC > 0 & dat1$logFC1] = "color1"
+dat1$color[dat1$logFC < 0] = "color2"
+dat1$color[dat1$logFC < -1] = "color3"
+write.table(dat1, file="MSBB_HIPP_Braak_CERAD_Pval.csv", sep=",", row.names=FALSE, col.names=TRUE, quote=FALSE)
+plot(dat1$logFC, dat1$logFC1)

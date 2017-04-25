c7e147e46daedba004102f2700781509a5ce0155
Sudeshna Das <sudeshna.das2@gmail.com>
2017-04-25 14:44:43 -0400

    New 0.001 csv file

--- sites/all/libraries/d3.scatterman/Script.R
+++ sites/all/libraries/d3.scatterman/Script.R
@@ -2,18 +2,9 @@ setwd("~/Sites/cats/sites/all/libraries/d3.scatterman")
 options(stringsAsFactors = FALSE)
 library(data.table)
 
-dat <- fread("ad_meta_analysis_filtered_0.001.tsv")
-head(dat)
-
-unique(dat$chr)
-dat$color[dat$chr %% 2 == 0] = "pink"
-dat$color[dat$chr %% 2 != 0] = "blue"
-unique(dat$color)
-write.table(dat, file="ad_meta_analysis_filtered_0.001.csv", sep=",", row.names=FALSE, col.names=TRUE, quote=FALSE)
-
 
 #dat <- fread("ad_meta_analysis_filtered_0.05.tsv")
-dat <- read.table("ad_meta_analysis_filtered_0.05.tsv", sep="\t", header=TRUE)
+dat <- read.table("ad_meta_analysis_filtered_0.001.tsv", sep="\t", header=TRUE)
 head(dat)
 
 unique(dat$chr)
@@ -46,7 +37,7 @@ sapply(chrs, function(chr) {
 })
 
 
-write.table(dat, file="ad_meta_analysis_filtered_0.05.csv", sep=",", row.names=FALSE, col.names=TRUE, quote=FALSE)
+write.table(dat, file="ad_meta_analysis_filtered_0.001.csv", sep=",", row.names=FALSE, col.names=TRUE, quote=FALSE)
 
 # Change P-value to Pvalue
 # Change to HGNC_nearest_gene_snpsnap_protein_coding to HGNC

92ba1263b7b12d8daacfeb6137c093fbad737a80
Emily Merrill <memerrill@gmail.com>
2017-04-26 13:36:32 -0400

    Tooltips added.

--- sites/all/libraries/d3.scatterman/scatterman.js
+++ sites/all/libraries/d3.scatterman/scatterman.js
@@ -78,7 +78,7 @@ var lasso_end = function() {
    var genes = lasso.items().filter(function(d) {return d.selected===true})
    genes = genes[0];
    console.debug("genes");
-   console.debug(genes[0].pval);
+   console.debug(genes);
 
    //nodeValue
 
@@ -123,7 +123,7 @@ var lasso_area = svg.append("rect")
 var lasso = d3.lasso()
       .closePathDistance(75) // max distance for the lasso loop to be closed
       .closePathSelect(true) // can items be selected by closing the path?
-      .hoverSelect(true) // can items by selected by hovering over them?
+      //.hoverSelect(true) // can items by selected by hovering over them?
       .area(lasso_area) // area where the lasso can be started
       .on("start",lasso_start) // lasso start function
       .on("draw",lasso_draw) // lasso draw function
@@ -183,18 +183,28 @@ d3.csv("/sites/all/libraries/d3.scatterman/ad_meta_analysis_filtered_0.001.csv",
   y_axis.selectAll(".tick")
         .classed("tick--one", function(d) { return Math.abs(d)<1e-6;  });
 
+  var tip = d3.tip()
+      .attr('class', 'd3-tip')
+      .html(function(d) { return (d.MarkerName + ": " + d.HGNC); })
+      .direction('nw')
+      .offset([0, 3]);
+
+  svg.call(tip);
+
   svg.selectAll(".dot")
       .data(data)
     .enter().append("circle")
-      .attr("id",function(d) {return (d.Pvalue + ": " + d.MarkerName + ": " + d.HGNC);})
-      .attr("pval",function(d) {return d.Pvalue;}) // added
+      .attr("id",function(d) {return ( d.MarkerName + ": " + d.HGNC + ", " + d.Pvalue.toFixed(2));})
       .attr("class", "dot")
       .attr("r", function(d) { return (d.Pvalue > 6 ? 3:2); })
       .attr("cx", function(d) { return x(d.cumulative_pos); })
       .attr("cy", function(d) { return y(d.Pvalue); })
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide)
       .style("fill", function(d) { return color(d.color); });
 
   lasso.items(d3.selectAll(".dot"));
+
 
 /*
   var legend = svg.selectAll(".legend")

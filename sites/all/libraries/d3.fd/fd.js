3553d9b1b5c42305c0a832eeb4e04ed74d99d313
Emily Merrill <memerrill@gmail.com>
2017-04-27 15:22:18 -0400

    New version of force directed with labels.

--- sites/all/libraries/d3.fd/fd.js
+++ sites/all/libraries/d3.fd/fd.js
@@ -15,117 +15,59 @@
    *       visualization to the DOM.
    */
   Drupal.d3.fd = function (select, settings) {
+    var width = 960,
+    height = 500
 
+    var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height);
 
-    var margin = {top: 20, right: 20, bottom: 30, left: 40},
-        width = 1200 - margin.left - margin.right,
-        height = 700 - margin.top - margin.bottom;
+    var force = d3.layout.force()
+        .gravity(0.05)
+        .distance(100)
+        .charge(-100)
+        .size([width, height]);
 
-    var svg = d3.select('#' + settings.id).append("svg")
-               .attr("width", width + margin.left + margin.right)
-               .attr("height", height + margin.top + margin.bottom);
-
-
-    var color = d3.scaleOrdinal(d3.schemeCategory20);
-
-    var fisheye = d3.fisheye.circular()
-        .radius(200)
-        .distortion(2);
-
-    var simulation = d3.forceSimulation()
-        .force("link", d3.forceLink().id(function(d) { return d.id; }))
-        .force("charge", d3.forceManyBody())
-        .force("center", d3.forceCenter(width / 2, height / 2));
-
-    d3.tsv("/sites/all/libraries/d3.fd/Tau.tsv", function(error, links) {
-     d3.csv("/sites/all/libraries/d3.fd/Tau-genes.csv", function(error, nodes) {
-
+    d3.json("graph.json", function(error, json) {
       if (error) throw error;
 
-      var link = svg.append("g")
-          .attr("class", "links")
-        .selectAll("line")
-        .data(links)
+      force
+          .nodes(json.nodes)
+          .links(json.links)
+          .start();
+
+      var link = svg.selectAll(".link")
+          .data(json.links)
         .enter().append("line")
-          .attr("stroke-width", function(d) { return d.value * 5; });
+          .attr("class", "link");
 
-      var node = svg.append("g")
-          .attr("class", "nodes")
-        .selectAll(".node")
-        .data(nodes)
+      var node = svg.selectAll(".node")
+          .data(json.nodes)
         .enter().append("g")
-        .call(d3.drag()
-              .on("start", dragstarted)
-              .on("drag", dragged)
-              .on("end", dragended));
+          .attr("class", "node")
+          .call(force.drag);
 
-      var circle=  node.append("circle")
-          .attr("r", 10)
-          .attr("fill", function(d) { return color(d.group); })
-
-      node.append("title")
-          .text(function(d) { return d.id; });
-
+      node.append("image")
+          .attr("xlink:href", "https://github.com/favicon.ico")
+          .attr("x", -8)
+          .attr("y", -8)
+          .attr("width", 16)
+          .attr("height", 16);
 
       node.append("text")
-          .attr("dx", function(d) { return d.x; })
-          .attr("dy", function(d) { return d.y; })
-          .style("text-anchor", "middle")
-          .style("font-size", function(d) { return 10 + "px" })
-          .text(function(d) { return d.id; });
+          .attr("dx", 12)
+          .attr("dy", ".35em")
+          .text(function(d) { return d.name });
 
-      simulation
-          .nodes(nodes)
-          .on("tick", ticked);
-
-      simulation.force("link")
-          .links(links);
-
-      function ticked() {
-        link
-            .attr("x1", function(d) { return d.source.x; })
+      force.on("tick", function() {
+        link.attr("x1", function(d) { return d.source.x; })
             .attr("y1", function(d) { return d.source.y; })
             .attr("x2", function(d) { return d.target.x; })
             .attr("y2", function(d) { return d.target.y; });
 
-        circle
-            .attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; });
-      }
-
-    /*
-  svg.on("mousemove", function() {
-       fisheye.focus(d3.mouse(this));
-
-       circle.each(function(d) { d.fisheye = fisheye(d); })
-          .attr("cx", function(d) { return d.fisheye.x; })
-          .attr("cy", function(d) { return d.fisheye.y; })
-          .attr("r", function(d) { return d.fisheye.z * 4.5; });
-
-       link.attr("x1", function(d) { return d.source.fisheye.x; })
-          .attr("y1", function(d) { return d.source.fisheye.y; })
-          .attr("x2", function(d) { return d.target.fisheye.x; })
-          .attr("y2", function(d) { return d.target.fisheye.y; });
+        node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
       });
- */
     });
-    });
-    function dragstarted(d) {
-      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-      d.fx = d.x;
-      d.fy = d.y;
-    }
-
-    function dragged(d) {
-      d.fx = d3.event.x;
-      d.fy = d3.event.y;
-    }
-
-    function dragended(d) {
-      if (!d3.event.active) simulation.alphaTarget(0);
-      d.fx = null;
-      d.fy = null;
-    }
 
 
   }

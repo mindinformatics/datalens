97a8bffbc89782b4cc0667e18f4c1174d79fa2f1
Emily Merrill <memerrill@gmail.com>
2017-09-28 15:00:12 -0400

    Updates to locus zoom

--- sites/all/libraries/d3.lzoom/lzoom.js
+++ sites/all/libraries/d3.lzoom/lzoom.js
@@ -16,18 +16,23 @@
    */
   Drupal.d3.lzoom = function (select, settings) {
 
-      window.module = "locuszoom";
+    window.module = "locuszoom";
 
-     console.log("I am here!");
+    console.log("I am here!");
+
+
     // Define Data Sources
+    apiBase = "https://portaldev.sph.umich.edu/api/v1/";
     var data_sources = new LocusZoom.DataSources()
       //.add("trait", ["AssociationLZ", {url: "sites/all/libraries/locuszoom/lzoom/IGAP_stage1_selected.json?", params: {analysis: 3, id_field: "MarkerName"}} ] );
-      .add("assoc", ["AssociationLZ", {url: "sites/all/libraries/locuszoom/staticdata/assoc_10_114550452-115067678.json?", params: {analysis: 3, id_field: "variant"} } ] )
-      .add("ld", ["LDLZ", {url: "sites/all/libraries/locuszoom/staticdata/ld_10_114550452-115067678.json?"}])
-      .add("gene", ["GeneLZ", { url: "sites/all/libraries/locuszoom/staticdata/genes_10_114550452-115067678.json?"}])
-      .add("recomb", ["RecombLZ", { url: "sites/all/libraries/locuszoom/staticdata/recomb_10_114550452-115067678.json?" }])
-      .add("constraint", ["GeneConstraintLZ", { url: "sites/all/libraries/locuszoom/staticdata/constraint_10_114550452-115067678.json?" }])
-      .add("intervals", ["IntervalLZ", { url: "sites/all/libraries/locuszoom/staticdata/intervals_10_114550452-115067678.json?"}]);
+      //21:27107568-27688738
+        .add("assoc", ["AssociationLZ", { url: apiBase + "statistic/single/", params: {analysis: 3, id_field: "variant"}}])
+        .add("ld", ["LDLZ", { url: apiBase + "pair/LD/" }])
+        .add("gene", ["GeneLZ", { url: apiBase + "annotation/genes/", params: {source: 2} }])
+        .add("recomb", ["RecombLZ", { url: apiBase + "annotation/recomb/results/", params: {source: 15} }])
+        .add("constraint", ["GeneConstraintLZ", { url: "http://exac.broadinstitute.org/api/constraint" }])
+        //.add("intervals", ["IntervalLZ", { url: apiBase + "annotation/intervals"}]);
+        .add("intervals", ["IntervalLZ", { url: "sites/all/libraries/locuszoom/staticdata/intervals_10_114550452-115067678.json?"}]);
 
     console.log("I am here 2!");
     // Get the standard assocation plot layout from LocusZoom's built-in layouts

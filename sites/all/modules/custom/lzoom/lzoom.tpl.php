b94381e222477ecaa98f7d7edc128f0f35b4b5eb
Sudeshna Das <sudeshna.das2@gmail.com>
2017-10-04 15:38:56 -0400

    Changed APIs

--- sites/all/modules/custom/lzoom/lzoom.tpl.php
+++ sites/all/modules/custom/lzoom/lzoom.tpl.php
@@ -4,5 +4,4 @@
  * Default theme file for d3 visualizations.
  */
  ?>
- <?php  var_dump("Template called"); ?>
 <div <?php print $attributes ?> data-region="10:114550452-115067678" class="<?php print implode(' ', $classes_array); ?>"></div>

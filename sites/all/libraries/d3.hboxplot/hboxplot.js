26b0d51bd4d39b19f7fbffde73937a4add35f05c
Emily Merrill <memerrill@gmail.com>
2017-11-16 16:57:26 -0500

    Forest plots now use our data file, with only our points, and corrected scaled and labeled x and y axis.  and pretty orange and red colors.

--- sites/all/libraries/d3.hboxplot/hboxplot.js
+++ sites/all/libraries/d3.hboxplot/hboxplot.js
@@ -15,9 +15,16 @@
    */
 Drupal.d3.hboxplot = function (select, settings) {
 
-d3.csv("/sites/all/themes/scf_theme/HBoxplot/hboxplot_data.csv", boxplot)
+d3.csv("/sites/all/themes/scf_theme/HBoxplot/CD33.csv", boxplot)
 
 function boxplot(data){
+
+  data.forEach(function(d) {
+            d.max = +d.max;
+            d.min = +d.min;
+            d.median = +d.median;
+            d.row = +d.row;
+          });
 
   var h = 500,
       w = 500;
@@ -31,11 +38,12 @@ function boxplot(data){
 
   d3.select('#' + settings.id).append("svg")
     .attr("height", h)
-    .attr("width", w);
+    .attr("width", 520);
 
 
   xScale = d3.scale.linear()
-    .domain([0,100]) // 0% to 100%
+    //.domain([0,100]) // 0% to 100%
+    .domain([d3.min(data, function(d){ return d.min }), d3.max(data, function(d){ return d.max })])
     .range([
       margin.left,
       w - margin.right
@@ -43,8 +51,8 @@ function boxplot(data){
 
   yScale = d3.scale.linear()
     .domain([
-      Number(d3.max(data, function(d){ return d.day })) + 1,
-      Number(d3.min(data, function(d){ return d.day })) - 1
+      Number(d3.max(data, function(d){ return d.row })) + 1,
+      Number(d3.min(data, function(d){ return d.row })) - 1
     ])
     .range([
       h - margin.bottom,
@@ -65,19 +73,29 @@ function boxplot(data){
     .attr("id", "xAxisG")
     .call(xAxis);
 
-  var days = data.map(function(d){return Number(d.day)});
+  var rows = data.map(function(d){return Number(d.row)});
+  var brain_regions = data.map(function(d){return (d.BrainRegion)});
 
   yAxis = d3.svg.axis()
     .scale(yScale)
     .orient("right")
     .tickSize(-12)
-    .tickValues(days);
+    .tickValues(rows)
+    .tickFormat(function(d,i) { return brain_regions[i] });
 
 
   d3.select("svg").append("g")
     .attr("transform", "translate(470,0)")
     .attr("id", "yAxisG")
     .call(yAxis);
+
+  d3.select("svg").append("line")
+    .attr("x1", xScale(1))
+    .attr("x2", xScale(1))
+    .attr("y1", 0)
+    .attr("y2", 470)
+    .style("stroke", "gray")
+    .style("stroke-width", "1px");
 
   d3.select("svg").selectAll("circle.median")
     .data(data)
@@ -86,7 +104,7 @@ function boxplot(data){
     .attr("class", "tweets")
     .attr("r", 5)
     .attr("cx", function(d) {return xScale(d.median)})
-    .attr("cy", function(d) {return yScale(d.day)})
+    .attr("cy", function(d) {return yScale(d.row)})
     .style("fill", "none");
 
   d3.select("svg").selectAll("g.box")
@@ -96,7 +114,7 @@ function boxplot(data){
     .attr("class", "box")
     .attr("transform", function(d){
       return "translate(" + xScale(d.median) + "," +
-        yScale(d.day) + ")"
+        yScale(d.row) + ")"
     })
     .each(function(d,i){
 
@@ -117,7 +135,7 @@ function boxplot(data){
         .attr("x2", xScale(d.max) - xScale(d.median))
         .attr("y1", -10)
         .attr("y2", 10)
-        .style("stroke", "black")
+        .style("stroke", "red")
         .style("stroke-width", "4px");
 
       d3.select(this)
@@ -127,10 +145,10 @@ function boxplot(data){
         .attr("x2", xScale(d.min) - xScale(d.median))
         .attr("y1", -10)
         .attr("y2", 10)
-        .style("stroke", "black")
+        .style("stroke", "orange")
         .style("stroke-width", "4px");
 
-      d3.select(this)
+    /*  d3.select(this)
         .append("rect")
         .attr("class", "range")
         .attr("x", xScale(d.q1) - xScale(d.median))
@@ -138,15 +156,16 @@ function boxplot(data){
         .attr("height", 20)
         .attr("width", xScale(d.q3) - xScale(d.q1))
         .style("fill", "white")
-        .style("stroke", "black")
-        .style("stroke-width", "2px");
+        .style("stroke", "blue")
+        .style("stroke-width", "2px"); */
 
       d3.select(this)
-        .append("line")
-        .attr("x1", 0)
-        .attr("x2", 0)
-        .attr("y1", -10)
-        .attr("y2", 10)
+        .append("rect")
+        .attr("x", 0)
+        .attr("y", -5)
+        .attr("height", 10)
+        .attr("width", 10)
+        .style("fill", "darkgray")
         .style("stroke", "darkgray")
         .style("stroke-width", "4px");
 

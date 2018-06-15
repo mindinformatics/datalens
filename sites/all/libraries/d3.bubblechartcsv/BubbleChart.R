22777fb9467692a4f064a946a34f858b70959322
Emily Merrill <memerrill@gmail.com>
2018-06-15 13:28:38 -0400

    Created output data frame called "dat" to write results to instead of file. Used "dat" because variable name used later in script.  However, renamed existing variable "dat" to "analysis" within function to prevent conflicts/confusion.

--- sites/all/libraries/d3.bubblechartcsv/BubbleChart.R
+++ sites/all/libraries/d3.bubblechartcsv/BubbleChart.R
@@ -20,9 +20,10 @@ genes=mouse_human[mouse_human$Homology.Type != "", "Human.Gene.Symbol"]
 
 analysisFile=analysisFile[(analysisFile$Contrast == "B3-B1"), ] # Choose just B3-B1 and RNA-Seq
 
-header=c("study", "bregion", "cbregion", "dtype", "contrast", "type", "GeneSymbol", "logFC", "FC", "P.Value", "adj.P.Val")
-
-write.table(t(header), "B3-B1-APOE.csv", sep ="," , col.names = F, row.names = F, na = "")
+# Create output dataframe.
+dat <- data.frame(study=character(),bregion=character(),cbregion=character(),dtype=character(),contrast=character(),
+                  type=character(), GeneSymbol=character(), logFC=double(), FC=double(),
+                  P.Value=double(), adj.P.Val=double(), stringsAsFactors=FALSE)
 
 sapply(seq_along(analysisFile$FileName), function(i) {if (analysisFile$PipelineScript[i] == "uArray_pipeline.R" | analysisFile$PipelineScript[i] == "RNASeq_pipeline.R") {
   print(i)
@@ -34,32 +35,32 @@ sapply(seq_along(analysisFile$FileName), function(i) {if (analysisFile$PipelineS
   if (analysisFile$PipelineScript[i] == "uArray_pipeline.R") { dtype <- "Microarray" }
   if (analysisFile$PipelineScript[i] == "RNASeq_pipeline.R") { dtype <- "RNA-Seq" }
   fname= paste("~/Dropbox (Partners HealthCare)/MSBB/Pipelines/",analysisFile$Folder[i], analysisFile$FileName[i], sep="")
-  dat<-fread(fname)
+  analysis<-fread(fname)
 
-  x<-grep("Gene.symbol",colnames(dat))
-  if(length(x) == 0) {colnames(dat)[x] <- "GeneSymbol"}
+  x<-grep("Gene.symbol",colnames(analysis))
+  if(length(x) == 0) {colnames(analysis)[x] <- "GeneSymbol"}
 
-  v1<-filter(dat, GeneSymbol %in% genes)
+  v1<-filter(analysis, GeneSymbol %in% genes)
 
   #v2<- v1[as.numeric(v1$P.Value)< 0.05,]
   v2<-do.call(rbind, lapply(split(v1,as.factor(v1$GeneSymbol)), function(x) {return(x[which.min(x$P.Value),])}))
 
   v3<-cbind(study, bregion, cbregion,  dtype, contrast, type, v2$GeneSymbol,v2$logFC, realfc(v2$logFC), v2$P.Value, v2$adj.P.Val)
+  
+  # Add results back; Note use of <<- assign back to global variable rather than use local copy within function
+  v4 = as.data.frame(v3)
+  colnames(v4) =c("study", "bregion", "cbregion", "dtype", "contrast", "type", "GeneSymbol", "logFC", "FC", "P.Value", "adj.P.Val")
+  dat <<- rbind(dat,v4)
 
-  write.table(v3, "B3-B1-APOE.csv", sep =",", append = T, col.names = F, row.names = F) # Change Here
-
-  return(dim(v3))
+  return(dim(v4))
   }
 }
 )
 
-fname="B3-B1-APOE.csv"
-#dat<-read.csv("", header = T)
+
 #sum(p.adjust(dat$P.Value, method="BH") < 0.30)
 
 ## Revised analysis
-dat<-read.csv(fname, header = T, stringsAsFactors = F)
-
 dat=dat[dat$type!="RC",]
 #dat=dat[dat$dtype != "RNA-Seq",] ??
 
